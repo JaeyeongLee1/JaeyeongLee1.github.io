06bb6cd970ada4a64714645cf1bedddc79b677cc
JaeyeongLee1 <65021103+JaeyeongLee1@users.noreply.github.com>
2026-08-12 03:36:12 +0900

    Remove border from PREP publication label

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ if (profilePhoto && photoFallback) {
     photoFallback.classList.add('visible');
   });
 }
+
+const prepLabel = document.querySelector(
+  '.publications-section > .publication-group:last-of-type .publication-label span'
+);
+
+if (prepLabel) {
+  prepLabel.style.border = 'none';
+}
